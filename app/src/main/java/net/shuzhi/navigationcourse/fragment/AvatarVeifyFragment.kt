ab3452b228cdc2a2299ace3043fc5943bbe41f6d
lsgdut <lhwywh@live.com>
2020-11-08 23:13:28 +0800

    feat(MainActivity):更新到第8节课 Jetpack组件Navigation-popUpTo和popUpToInclusive

--- app/src/main/java/net/shuzhi/navigationcourse/fragment/AvatarVeifyFragment.kt
+++ app/src/main/java/net/shuzhi/navigationcourse/fragment/AvatarVeifyFragment.kt
@@ -2,24 +2,23 @@ package net.shuzhi.navigationcourse.fragment
 
 import android.view.View
 import androidx.navigation.fragment.findNavController
-import kotlinx.android.synthetic.main.fragment_login.view.*
-import kotlinx.android.synthetic.main.fragment_register.view.*
+import kotlinx.android.synthetic.main.fragment_avater_verify.view.*
 import net.shuzhi.navigationcourse.R
 import net.shuzhi.navigationcourse.base.BaseFragment
 
 /**
  * @author 梁爽
- * @create 2020/11/7 17:16
+ * @create 2020/11/8 23:00
  */
-class RegisterFragment :BaseFragment(){
+class AvatarVeifyFragment :BaseFragment() {
     override fun getLayoutResId(): Int {
-        return R.layout.fragment_register
+        return R.layout.fragment_avater_verify
     }
 
     override fun initView(rootView: View) {
         super.initView(rootView)
-        rootView.toAvatarVerify.setOnClickListener {
-            findNavController().navigate(R.id.to_verify_fragment)
+        rootView.avatarVerifyButton.setOnClickListener{
+            findNavController().navigate(R.id.to_login_page)
         }
     }
 }
